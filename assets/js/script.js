ac3f356aa63168e1ad6c20fbc2b3753f1537a749
Ilham Fathullah <ilhamfathullah04@gmail.com>
2024-07-17 13:59:04 +0700

    day 3 - form submission

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function getData() {
+  let name = document.getElementById("nameInput").value;
+  let email = document.getElementById("emailInput").value;
+  let position = document.getElementById("position").value;
+  let address = document.getElementById("address").value;
+
+  console.log(name);
+  console.log(email);
+  console.log(position);
+  console.log(address);
+
+  //   tipe data, variable, function
+  //    conditional, tipe data array dan object
+}
