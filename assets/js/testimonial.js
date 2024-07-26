37d8fb262401b663b6fdaa2d92373b3d8c6478e8
suryaelidanto <suryaelidanto@gmail.com>
2024-07-26 15:15:00 +0700

    ffeat: asynchronous concept

--- assets/js/testimonial.js
+++ assets/js/testimonial.js
@@ -1,59 +1,70 @@
-const testimonials = [
-  {
-    image:
-      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600",
-    content: "Mantap bang!",
-    author: "Surya",
-    rating: 1,
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600",
-    content: "Mantap keren sekali!",
-    author: "Alfi Dharmawan",
-    rating: 2,
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600",
-    content: "Mantap keren sekali!",
-    author: "Alfi Dharmawan",
-    rating: 2,
-  },
-];
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-function allTestimonial() {
-  const testimonialHTML = testimonials.map((testimonial) => {
-    return `<div class="testimonial">
-                <img
-                  src="${testimonial.image}"
-                  class="profile-testimonial"
-                />
-                <p class="quote">${testimonial.content}</p>
-                <p class="author">- ${testimonial.author}</p>
-              </div>`;
+    xhr.open("GET", url, true);
+
+    xhr.onerror = () => {
+      reject("Network error!");
+    };
+
+    xhr.onload = () => {
+      resolve(JSON.parse(xhr.responseText));
+    };
+
+    xhr.send();
   });
-
-  document.getElementById("testimonials").innerHTML = testimonialHTML.join(" ");
 }
 
-function filterTestimonial(rating) {
-  const filteredTestimonialByRating = testimonials.filter((testimonial) => {
-    return testimonial.rating == rating;
-  });
+async function allTestimonial() {
+  try {
+    const testimonials = await fetchUrl(
+      "https://api.npoint.io/e320fa70a61a3c02310b"
+    );
 
-  const testimonialHTML = filteredTestimonialByRating.map((testimonial) => {
-    return `<div class="testimonial">
-                    <img
-                      src="${testimonial.image}"
-                      class="profile-testimonial"
-                    />
-                    <p class="quote">${testimonial.content}</p>
-                    <p class="author">- ${testimonial.author}</p>
-                  </div>`;
-  });
+    const testimonialHTML = testimonials.map((testimonial) => {
+      return `<div class="testimonial">
+                  <img
+                    src="${testimonial.image}"
+                    class="profile-testimonial"
+                  />
+                  <p class="quote">${testimonial.content}</p>
+                  <p class="author">- ${testimonial.author}</p>
+                </div>`;
+    });
 
-  document.getElementById("testimonials").innerHTML = testimonialHTML.join(" ");
+    document.getElementById("testimonials").innerHTML =
+      testimonialHTML.join(" ");
+  } catch (error) {
+    alert(error);
+  }
+}
+
+async function filterTestimonial(rating) {
+  try {
+    const testimonials = await fetchUrl(
+      "https://api.npoint.io/e320fa70a61a3c02310b"
+    );
+  
+    const filteredTestimonialByRating = testimonials.filter((testimonial) => {
+      return testimonial.rating == rating;
+    });
+  
+    const testimonialHTML = filteredTestimonialByRating.map((testimonial) => {
+      return `<div class="testimonial">
+                      <img
+                        src="${testimonial.image}"
+                        class="profile-testimonial"
+                      />
+                      <p class="quote">${testimonial.content}</p>
+                      <p class="author">- ${testimonial.author}</p>
+                    </div>`;
+    });
+  
+    document.getElementById("testimonials").innerHTML = testimonialHTML.join(" ");
+  } catch(error) {
+    alert(error)
+  }
 }
 
 allTestimonial();
