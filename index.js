35864310912b385e16b4993cccfb1168d989a711
Sundus <pcinta0@gmail.com>
2024-08-02 15:19:25 +0700

    day 9

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const session = require("express-session");
+const flash = require("express-flash");
+const multer = require("multer");
 
 const db = require("./src/lib/db");
 const { QueryTypes } = require("sequelize");
 
+const upload = multer({
+   storage: multer.diskStorage({
+      destination: (req, file, cb) => {
+         cb(null, "uploads/");
+      },
+      filename: (req, file, cb) => {
+         cb(null, Date.now() + file.originalname);
+      },
+   }),
+});
+
 app.set("view engine", "hbs");
 app.set("views", "views");
-
+app.set("trust proxy", 1);
 // setup untuk bisa mengakses static file
 app.use("/assets", express.static("assets"));
+app.use("/uploads", express.static("uploads"));
+app.use(
+   session({
+      secret: "dumswey",
+      cookie: { maxAge: 3600000, secure: false, httpOnly: true },
+      saveUninitialized: true,
+      resave: false,
+      store: new session.MemoryStore(),
+   })
+);
+app.use(flash());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,13 +48,18 @@ app.get("/", (req, res) => {
    });
 });
 app.get("/blog", renderBlog);
-app.post("/blog", addBlog);
+app.post("/blog", upload.single("image"), addBlog);
 app.get("/contact", renderContact);
 app.get("/testimonial", renderTestimonial);
 app.get("/blog-detail/:blog_id", renderBlogDetail);
 app.get("/edit-blog/:blog_id", renderEditBlog);
 app.post("/edit-blog/:blog_id", editBlog);
 app.get("/delete-blog/:blog_id", deleteBlog);
+app.get("/login", renderLogin);
+app.post("/login", login);
+app.get("/register", renderRegister);
+app.post("/register", register);
+app.get("/logout", logout);
 
 function renderContact(req, res) {
    res.render("contact");
@@ -40,17 +70,23 @@ function renderTestimonial(req, res) {
 }
 
 async function renderBlog(req, res) {
+   const isLogin = req.session.isLogin;
+
    const query = `SELECT * FROM blog;`;
    const result = await db.query(query, { type: QueryTypes.SELECT });
 
    res.render("blog", {
       data: result,
+      isLogin: isLogin,
+      user: req.session.user,
    });
 }
 
 async function addBlog(req, res) {
    try {
+      const user = req.session.user;
       console.log(req.body);
+      console.log(req.file);
 
       // const newBlog = {
       //    id: blogs.length + 1,
@@ -64,9 +100,9 @@ async function addBlog(req, res) {
 
       const query = `
       INSERT INTO blog
-      (title, content, created_at, author)
+      (title, content, created_at, author,image)
       VALUES
-      ('${req.body.title}', '${req.body.content}', NOW(), 'Cundus')
+      ('${req.body.title}', '${req.body.content}', NOW(), '${user.fullname}', '${req.file.filename}')
       `;
 
       await db.query(query);
@@ -141,6 +177,85 @@ async function deleteBlog(req, res) {
    await db.query(query);
 
    res.redirect("/blog");
+}
+
+function renderLogin(req, res) {
+   const isLogin = req.session.isLogin;
+   if (isLogin) {
+      req.flash("error", "anda harus login terlebih dahulu");
+      res.redirect("/blog");
+      return;
+   }
+
+   res.render("login");
+}
+
+async function login(req, res) {
+   try {
+      const query = `
+      SELECT * FROM user
+      WHERE
+      email = '${req.body.email}'
+      AND
+      password = '${req.body.password}'
+      `;
+      const existUser = await db.query(query, {
+         type: QueryTypes.SELECT,
+      });
+
+      if (existUser.length == 0) {
+         req.flash("error", "login gagal");
+         res.redirect("/login");
+         return;
+      }
+
+      req.session.user = existUser[0];
+      req.session.isLogin = true;
+
+      req.flash("succes", "login sukses");
+      res.redirect("/blog");
+   } catch (error) {
+      console.log(error);
+
+      res.redirect("/login");
+   }
+}
+
+function renderRegister(req, res) {
+   console.log(req.session);
+   const isLogin = req.session.isLogin;
+   if (isLogin) {
+      req.flash("error", "anda harus login terlebih dahulu");
+      res.redirect("/blog");
+      return;
+   }
+
+   res.render("register");
+}
+
+async function register(req, res) {
+   try {
+      const query = `
+      INSERT INTO user
+      (fullname, email, password)
+      VALUES
+      ('${req.body.fullname}', '${req.body.email}', '${req.body.password}')
+      `;
+
+      await db.query(query, { type: QueryTypes.INSERT });
+      console.log("SUKSES REGIST");
+      req.flash("succes", "register berjalan");
+      res.redirect("/register");
+   } catch (error) {
+      console.log("error", error);
+
+      res.redirect("/register");
+   }
+}
+
+function logout(req, res) {
+   req.session.destroy();
+   res.redirect("/login");
 }
 
 // akhir routes
